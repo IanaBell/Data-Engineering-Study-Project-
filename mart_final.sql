7306353c094fc8bfb2ebcdd6b86d42b35286fb96
Iana <beloyana@gmail.com>
2025-03-12 17:48:27 +0300

    Update mart_final.sql

--- mart_final.sql
+++ mart_final.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION std7_111.f_load_mart_final(p_start_date varchar, p_end_date varchar)
+CREATE OR REPLACE FUNCTION default.f_load_mart_final(p_start_date varchar, p_end_date varchar)
 RETURNS int4
 	LANGUAGE plpgsql
 	VOLATILE
@@ -12,14 +12,16 @@ DECLARE
 	v_end_date date;
 BEGIN 
 
-	PERFORM std7_111.f_load_write_log(p_log_type := 'INFO',
-									  p_log_message := 'Start f_load_mart_final',
-									  p_location := 'Sales final mart calculation');
+	PERFORM default.f_load_write_log(p_log_type := 'INFO',
+					 p_log_message := 'Start f_load_mart_final',
+					 p_location := 'Sales final mart calculation');
 
-	v_start_date = date_trunc('day',to_date(p_start_date, 'YYYYMMDD'));
-	v_end_date = date_trunc('day',to_date(p_end_date, 'YYYYMMDD'));
+	v_start_date = to_date(p_start_date, 'YYYYMMDD');
+	v_end_date = to_date(p_end_date, 'YYYYMMDD');
+	
+	EXECUTE 'DROP TABLE IF EXISTS default.mart_final';
 
-	v_sql := 'CREATE TABLE std7_111.mart_final (
+	v_sql := 'CREATE TABLE default.mart_final (
 		plant varchar(4),
 		plant_txt text,
 		turnover numeric(17, 2),
@@ -44,13 +46,13 @@ BEGIN
 		DISTRIBUTED RANDOMLY';
 	EXECUTE v_sql;
 	
-	EXECUTE 'INSERT INTO std7_111.mart_final WITH bills_df AS (
+	EXECUTE 'INSERT INTO default.mart_final WITH bills_df AS (
 					SELECT  bh.plant AS plant, 
 							SUM(bi.rpa_sat) AS turnover,
 							SUM(bi.qty) AS sold_materials,
 							COUNT(DISTINCT bi.billnum) AS bills_num
-					FROM std7_111.bills_item bi
-					JOIN std7_111.bills_head bh
+					FROM default.bills_item bi
+					JOIN default.bills_head bh
 					ON bi.billnum = bh.billnum
 					WHERE bi.calday BETWEEN '''||v_start_date||''' AND '''||v_end_date||'''
 					GROUP BY 1				
@@ -58,10 +60,11 @@ BEGIN
 				 traffic_df AS (
 					SELECT  plant,
 							SUM(quantity) AS traffic
-					FROM std7_111.traffic
+					FROM default.traffic tr
+					WHERE tr.date BETWEEN '''||v_start_date||''' AND '''||v_end_date||'''
 					GROUP BY 1
 					),
-				 coupons_staging AS (
+				 coupons_stg AS (
 					SELECT  c.plant AS plant,
 							c.material AS material,
 							CASE 
@@ -69,16 +72,16 @@ BEGIN
 								WHEN p.promo_type = ''002'' THEN (p.promo_size::numeric * bi.rpa_sat) / (bi.qty * 100)
 							END AS discount,
 							ROW_NUMBER() OVER (PARTITION BY c.coupon_num) AS rank
-					FROM std7_111.coupons c
-					JOIN std7_111.promos p ON c.coupon_promo = p.promo_id
-					JOIN std7_111.bills_item bi ON c.billnum = bi.billnum AND c.material = bi.material
+					FROM default.coupons c
+					JOIN default.promos p ON c.coupon_promo = p.promo_id
+					JOIN default.bills_item bi ON c.billnum = bi.billnum AND c.material = bi.material
 					WHERE c.calday BETWEEN '''||v_start_date||''' AND '''||v_end_date||'''
 					),
 				 coupons_df AS (
 					SELECT  plant,
 							COUNT(material) AS promo_materials,
 							SUM(discount) AS coupon_discount
-					FROM coupons_staging
+					FROM coupons_stg
 					WHERE rank = 1
 					GROUP BY 1
 					)
@@ -100,13 +103,15 @@ BEGIN
 						WHEN td.traffic = 0 THEN 0
 						ELSE ROUND(bd.turnover::numeric / td.traffic, 1)
 					END AS revenue_per_client_avg 
-			FROM  std7_111.stores s
+			FROM  default.stores s
 			LEFT JOIN bills_df bd ON s.plant = bd.plant
 			LEFT JOIN traffic_df td ON s.plant = td.plant
 			LEFT JOIN coupons_df cd ON s.plant = cd.plant
 			ORDER BY 1';
 
-	 EXECUTE 'SELECT COUNT(*) FROM std7_111.mart_final' INTO v_return;
+	 EXECUTE 'SELECT COUNT(*) FROM default.mart_final' INTO v_return;
 
      RETURN v_return;
 END;
+$$
+EXECUTE ON ANY;
